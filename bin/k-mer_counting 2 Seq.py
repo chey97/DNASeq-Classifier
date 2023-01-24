141c8614dd3579fe1ab0eee573c637d9e8e2a315
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2023-01-24 15:40:32 -0500

    join the list of k-mers

--- bin/k-mer_counting 2 Seq.py
+++ bin/k-mer_counting 2 Seq.py
@@ -23,3 +23,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer()
 X = cv.fit_transform([joined_sentence, sentence1, sentence2]).toarray()
 print(X)
+
+#code is tokenizing the input sentences, 
+# counting the occurrences of each word in the vocabulary, 
+# and returning the token count as a matrix.
